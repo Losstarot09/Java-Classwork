0d78bc917ed4fc2af68386501571676453812357
Artūrs Lobanovs <lobanovs.arturs@gmail.com>
2021-02-27 08:30:09 +0200

    task 1 CheckPassFail done

--- src/com/rcs/Classwork/Day5/CheckPassFail.java
+++ src/com/rcs/Classwork/Day5/CheckPassFail.java
@@ -1,8 +1,21 @@
 package com.rcs.Classwork.Day5;
 
+import java.util.Scanner;
+
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.print("Lūdzu, ievadiet savu atzīmi (0-100): ");
+        Scanner read = new Scanner(System.in);
+        int mark = read.nextInt();
+        read.close();
+
+        if ( mark > 50) {
+            System.out.print("PASS ");
+        } else {
+            System.out.print("FAIL ");
+        }
+
+        System.out.println("DONE");
     }
 }
