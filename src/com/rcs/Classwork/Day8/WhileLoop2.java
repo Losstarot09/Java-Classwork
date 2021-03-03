10655ca4f6a582bdf9f560a462affa451c474619
Artūrs Lobanovs <lobanovs.arturs@gmail.com>
2021-03-03 19:13:23 +0200

    ClassWork day 8 task 2 done

--- src/com/rcs/Classwork/Day8/WhileLoop2.java
+++ src/com/rcs/Classwork/Day8/WhileLoop2.java
@@ -1,7 +1,18 @@
 package com.rcs.Classwork.Day8;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class WhileLoop2 {
     public static void main(String[] args) {
         System.out.println("Uzdevums 2");
+        System.out.print("Lūdzu, ievadiet tekstu: ");
+        Scanner read = new Scanner(System.in);
+        String text = read.nextLine().trim();
+        read.close();
+        int i = 0;
+        while (i < text.length()) {
+            System.out.println(text.toUpperCase().charAt(i++));
+        }
     }
 }
