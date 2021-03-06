b4e24a870e75289f01cd09df543dc613dd29a5fa
Artūrs Lobanovs <lobanovs.arturs@gmail.com>
2021-03-06 10:31:24 +0200

    ClassWork day 9 task 1-3 done

---
+++ src/com/rcs/Classwork/Day9/WhileLoop3.java
@@ -0,0 +1,17 @@
+package com.rcs.Classwork.Day9;
+
+import java.util.Scanner;
+
+public class WhileLoop3 {
+    public static void main(String[] args) {
+        System.out.println("Uzdevums 3");
+        System.out.print("Lūdzu ievadiet veselo skaitļi, kas ir lielaks par 0: ");
+        Scanner read = new Scanner(System.in);
+        int number = read.nextInt();
+        read.close();
+        int i = 1;
+        do {
+            System.out.printf("%d * %d = %d%n", number, i, number * i++);
+        } while (i <= number);
+    }
+}
