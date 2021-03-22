d6b1cc8d68fb47f4e170261a464a9e3994e776d9
Artūrs Lobanovs <lobanovs.arturs@gmail.com>
2021-03-22 19:33:28 +0200

    ClassWork day 14 task 2 done

--- src/com/rcs/Classwork/Day14/ReplaceListItems.java
+++ src/com/rcs/Classwork/Day14/ReplaceListItems.java
@@ -1,8 +1,28 @@
 package com.rcs.Classwork.Day14;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ReplaceListItems {
 
     public static void main(String[] args) {
+        System.out.println("Uzdevums 2");
+        List<Integer> numberList = new ArrayList<>();
+        Random rnd = new Random();
+        int size = rnd.nextInt(16) + 5;
 
+        for (int i = 0; i < size; i++) {
+            numberList.add(rnd.nextInt(101));
+            if (numberList.get(i) <= 39) {
+                numberList.set(i, 0);
+            }
+            System.out.printf("%3d", numberList.get(i));
+            if ( i != size) {
+                System.out.print("; ");
+            } else {
+                System.out.println();
+            }
+        }
     }
 }
